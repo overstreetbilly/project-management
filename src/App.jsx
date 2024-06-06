fae28e97ad4fbfeaec78578c96a308a25d09ace6
Billy Overstreet <overstreetbilly@gmail.com>
2024-06-06 16:19:42 -0600

    Removed a simple line of text

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ function App() {
     return (
         <>
             <h1 className='text-3xl font-bold underline'>Hello World</h1>
-            <p>A simple line of text</p>
         </>
     );
 }
