117e43ef0f207aee10b41d683fe1083a20dd6042
Billy Overstreet <overstreetbilly@gmail.com>
2024-06-06 16:11:26 -0600

    removed unused files and css rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
-
 import "./App.css";
 
 function App() {
     return (
         <>
             <h1 className='text-3xl font-bold underline'>Hello World</h1>
+            <p>A simple line of text</p>
         </>
     );
 }
