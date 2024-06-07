a9fdfa9bcce1b2d43457891f238cecb9fac286ea
Billy Overstreet <overstreetbilly@gmail.com>
2024-06-07 16:55:22 -0600

    Added logic to cancel add new project

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,7 +2,7 @@ import Input from "./Input";
 import { useRef } from "react";
 import Modal from "./Modal";
 
-const NewProject = ({ onAdd }) => {
+const NewProject = ({ onAdd, onCancel }) => {
     const modal = useRef();
     const title = useRef();
     const description = useRef();
@@ -32,9 +32,13 @@ const NewProject = ({ onAdd }) => {
     return (
         <>
             <Modal ref={modal} buttonCaption='Close'>
-                <h2>Invalid Input</h2>
-                <p>Oops... looks like you forgot to enter a value.</p>
-                <p>
+                <h2 className='text-xl font-bold text-stone-700 my-4'>
+                    Invalid Input
+                </h2>
+                <p className='text-stone-600 mb-4'>
+                    Oops... looks like you forgot to enter a value.
+                </p>
+                <p className='text-stone-600 mb-4'>
                     Please make sure you provide a valid value for every input
                     field.
                 </p>
@@ -42,7 +46,10 @@ const NewProject = ({ onAdd }) => {
             <div className='w-[35rem] mt-16'>
                 <menu className='flex items-center justify-end gap-4 my-4'>
                     <li>
-                        <button className='text-stone-800 hover:text-stone-950'>
+                        <button
+                            className='text-stone-800 hover:text-stone-950'
+                            onClick={onCancel}
+                        >
                             Cancel
                         </button>
                     </li>
