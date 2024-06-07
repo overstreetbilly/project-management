31958cb6cd73c163413f7c033bcf9a92942c236b
Billy Overstreet <overstreetbilly@gmail.com>
2024-06-07 16:18:05 -0600

    Added modal component to show if input fields are empty.

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,9 @@
 import Input from "./Input";
 import { useRef } from "react";
+import Modal from "./Modal";
 
 const NewProject = ({ onAdd }) => {
+    const modal = useRef();
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
@@ -11,7 +13,14 @@ const NewProject = ({ onAdd }) => {
         const enteredDescription = description.current.value;
         const enteredDueDate = dueDate.current.value;
 
-        //Add Validation... show modal
+        if (
+            enteredTitle.trim() === "" ||
+            enteredDescription.trim() === "" ||
+            enteredDueDate.trim() === ""
+        ) {
+            modal.current.open();
+            return;
+        }
 
         onAdd({
             title: enteredTitle,
@@ -21,28 +30,38 @@ const NewProject = ({ onAdd }) => {
     }
 
     return (
-        <div className='w-[35rem] mt-16'>
-            <menu className='flex items-center justify-end gap-4 my-4'>
-                <li>
-                    <button className='text-stone-800 hover:text-stone-950'>
-                        Cancel
-                    </button>
-                </li>
-                <li>
-                    <button
-                        onClick={handleSave}
-                        className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'
-                    >
-                        Save
-                    </button>
-                </li>
-            </menu>
-            <div>
-                <Input type='text' ref={title} label='Title' />
-                <Input ref={description} label='Description' textarea />
-                <Input type='date' ref={dueDate} label='Due Date' />
+        <>
+            <Modal ref={modal} buttonCaption='Close'>
+                <h2>Invalid Input</h2>
+                <p>Oops... looks like you forgot to enter a value.</p>
+                <p>
+                    Please make sure you provide a valid value for every input
+                    field.
+                </p>
+            </Modal>
+            <div className='w-[35rem] mt-16'>
+                <menu className='flex items-center justify-end gap-4 my-4'>
+                    <li>
+                        <button className='text-stone-800 hover:text-stone-950'>
+                            Cancel
+                        </button>
+                    </li>
+                    <li>
+                        <button
+                            onClick={handleSave}
+                            className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'
+                        >
+                            Save
+                        </button>
+                    </li>
+                </menu>
+                <div>
+                    <Input type='text' ref={title} label='Title' />
+                    <Input ref={description} label='Description' textarea />
+                    <Input type='date' ref={dueDate} label='Due Date' />
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
